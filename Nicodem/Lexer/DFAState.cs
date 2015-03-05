d96e6856fc129e18c8c584fd3df3f3da7346a63d
karolbanys <karol.banys@gmail.com>
2015-03-05 12:19:08 +0100

    Add definition of DFA and DFAState classes

---
+++ Nicodem/Lexer/DFAState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nicodem.Lexer
+{
+    class DFAState
+    {
+        uint Accepting {get;}
+        KeyValuePair<char,DFAState>[] Transitions;
+    }
+}
