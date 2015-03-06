50d33187989fc87b2c31a2531d1ee518d7ce138c
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2015-03-06 19:32:14 +0100

    Testy - Lexer

---
+++ Nicodem/Lexer.Tests/LexerTests.cs
@@ -0,0 +1,15 @@
+﻿using Nicodem.Lexer;
+using NUnit.Framework;
+
+namespace Lexer.Tests
+{
+    [TestFixture]
+    internal class LexerTests
+    {
+        [Test]
+        private void LexerEmpty()
+        {
+            var lexer = new Nicodem.Lexer.Lexer(new RegEx[0]);
+        }
+    }
+}
