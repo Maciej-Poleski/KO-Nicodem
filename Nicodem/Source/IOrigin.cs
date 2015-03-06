c3dcdffc522b676a3f0657d82dde67cea7d1b5f8
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2015-03-06 13:21:09 +0100

    Źródło powinno umieć tworzyć fragment mając zadany przedział lokacji

--- Nicodem/Source/IOrigin.cs
+++ Nicodem/Source/IOrigin.cs
@@ -7,5 +7,16 @@
     {
         TLocation begin { get; }
         IOriginReader<TOrigin, TMemento, TLocation, TFragment> GetReader();
+
+        /// <summary>
+        ///     Tworzy fragment reprezentujący przedział od lokacji <paramref name="from" /> do lokacji <paramref name="to" />.
+        ///     <remarks>
+        ///         Obie lokacje powinny odnosić się do tego źródła.
+        ///     </remarks>
+        /// </summary>
+        /// <param name="from">Początek fragmentu</param>
+        /// <param name="to">Koniec fragmentu (znak wskazywany przez tą lokacje powinien NIE być częścią wynikowego fragmentu)</param>
+        /// <returns>Fragment reprezentujący dany przedział</returns>
+        TFragment MakeFragment(TLocation from, TLocation to);
     }
 }
