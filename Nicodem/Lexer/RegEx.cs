318f027d5db59a3aa811c98e35c2c9a001d9bd8c
Patryk Mikos <usciskajkaktusa@gmail.com>
2015-03-08 23:46:56 +0100

    RegEx IEquatable - might be helpful

--- Nicodem/Lexer/RegEx.cs
+++ Nicodem/Lexer/RegEx.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 
 namespace Nicodem.Lexer
 {
-	public abstract class RegEx : IComparable<RegEx>
+	public abstract class RegEx : IComparable<RegEx>, IEquatable<RegEx>
 	{
 		#region IComparable implementation
+
 		public virtual int CompareTo (RegEx other)
 		{
 			//Derived classes should override this method
 			throw new NotImplementedException ();
 		}
+
+		#endregion
+
+		#region IEquatable implementation
+
+		public bool Equals (RegEx other)
+		{
+			// should be correlated with compareTo
+			return CompareTo (other) == 0;
+		}
+
+		#endregion
+
+		#region standard methods
+
+		public override bool Equals (object obj)
+		{
+			var regex = obj as RegEx;
+			return regex != null && Equals (regex);
+		}
+
+		public override int GetHashCode ()
+		{
+			return base.GetHashCode ();
+		}
+
 		#endregion
 
         public virtual bool HasEpsilon()
