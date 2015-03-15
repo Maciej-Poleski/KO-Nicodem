fab0bef53d09604826c4d3151d5458d604a80ff5
Marcin Ziemiński <zieminn@gmail.com>
2015-03-15 02:09:14 +0100

    Extracted MemoizedInput to separate file and wrote unit tests for it.

--- Nicodem/Parser/LlParser.cs
+++ Nicodem/Parser/LlParser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Collections;
 using Nicodem.Lexer;
+using Nicodem.Core;
 
 namespace Nicodem.Parser
 {
@@ -15,7 +16,7 @@ namespace Nicodem.Parser
 			if(grammar.HasLeftRecursion()) {
 				// TODO fail somehow
 			}
-			_grammar = ParserUtils.SimplifyGrammar(grammar);
+            _grammar = grammar;
 		}
 
 		// TODO specify what happens when parser fails to eat the word
@@ -157,148 +158,4 @@ namespace Nicodem.Parser
 		}
 
 	}
-
-	internal class MemoizedInput<T>
-	{
-
-		private List<T> elements;
-		private IEnumerator<T> current;
-
-		public MemoizedInput(IEnumerable<T> input)
-		{
-			current = input.GetEnumerator();
-			if(current.MoveNext()) {
-				elements.Add(current.Current);
-			}
-		}
-
-		public Iterator Begin
-		{
-			get {
-				if(elements.Any()) {
-					return new Iterator(this, 0);
-				} else {
-					return End;
-				}
-			}
-		}
-
-		public Iterator End
-		{
-			get {
-				return new Iterator(this, -1);
-			}
-		}
-
-		public Iterator At(int pos)
-		{
-			if(pos > elements.Count || pos < -1) {
-				return End;
-			} else {
-				return new Iterator(this, pos);
-			}
-		}
-
-		private bool GoNext() {
-			if(current.MoveNext()) {
-				elements.Add(current.Current);
-				return true;
-			} else {
-				return false;
-			}
-		}
-
-		public struct Iterator : IComparable<Iterator>
-		{
-
-			public int Pos { get; private set; }
-			private readonly MemoizedInput<T> _parent;
-
-			public Iterator(MemoizedInput<T> parent, int pos)
-				: this()
-			{
-				this.Pos = pos;
-				_parent = parent;
-			}
-
-			public T Current
-			{
-				get {
-					return _parent.elements[Pos];
-				}
-			}
-
-			public Iterator Next()
-			{
-				if(Pos + 1 < _parent.elements.Count) {
-					_parent.GoNext();
-				}
-				return new Iterator(_parent, Pos + 1);
-			}
-
-			#region IComparable implementation
-
-			public int CompareTo(Iterator other)
-			{
-				if(Pos == -1) {
-					if(other.Pos == -1) {
-						return 0;
-					} else {
-						return 1;
-					}
-				} else if(other.Pos == -1) {
-					return -1;
-				} else {
-					return Pos - other.Pos;
-				}
-			}
-
-			#endregion
-
-			public static bool operator==(Iterator a, Iterator b)
-			{
-				return a.Pos == b.Pos && a._parent == b._parent;
-			}
-
-			public static bool operator!=(Iterator a, Iterator b)
-			{
-				return !(a.Pos == b.Pos);
-			}
-
-			public static bool operator>(Iterator a, Iterator b)
-			{
-				return a.CompareTo(b) > 0;
-			}
-
-			public static bool operator>=(Iterator a, Iterator b)
-			{
-				return a.CompareTo(b) > 0 || a == b;
-			}
-
-			public static bool operator<(Iterator a, Iterator b)
-			{
-				return !(a >= b);
-			}
-
-			public static bool operator<=(Iterator a, Iterator b)
-			{
-				return !(a > b);
-			}
-
-			public override bool Equals(Object other)
-			{
-				if(other is Iterator) {
-					return this == (Iterator) other;
-				} else {
-					return false;
-				}
-			}
-
-			public override int GetHashCode()
-			{
-				return Pos.GetHashCode() + _parent.GetHashCode();
-			}
-		}
-	}
 }
-
