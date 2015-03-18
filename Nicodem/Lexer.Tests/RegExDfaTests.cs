53938dd8c4cb36dc0cf502e5f7c04dfca973b7df
karolbanys <karol.banys@gmail.com>
2015-03-18 12:58:29 +0100

    Sample tests to RegExDfa

--- Nicodem/Lexer.Tests/RegExDfaTests.cs
+++ Nicodem/Lexer.Tests/RegExDfaTests.cs
@@ -11,11 +11,48 @@ namespace Lexer.Tests
     [TestFixture]
     class RegExDfaTests
     {
+
+        public void CheckNullTransitionTests(RegExDfa<char> regExDfa)
+        {
+            var queue = new Queue<DFAState<char>>();
+            var visited = new Dictionary<DFAState<char>, int>();
+
+            DFAState<char> start = regExDfa.Start;
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var state = queue.Dequeue();
+                if (visited.ContainsKey(state))
+                    continue;
+                visited[state] = 1;
+
+                Assert.IsNotNull(state.Transitions);
+
+                foreach (var transition in state.Transitions)
+                {
+                    Assert.IsNotNull(transition.Value);
+                    if(!visited.ContainsKey(transition.Value))
+                        queue.Enqueue(transition.Value);
+                }
+
+            }
+
+        }
+
         //Create Dfa from empty RegEx
         [Test]
-        public void EmptyRegexTests()
+        public void EmptyRegExTests()
         {
-            RegexDfa<char> regEx = new RegexDfa<char>(RegExFactory.Empty<char>(), 1);
+            RegExDfa<char> regExDfa = new RegExDfa<char>(RegExFactory.Empty<char>(), 1);
+            CheckNullTransitionTests(regExDfa);
+        }
+
+        [Test]
+        public void EpsilonRegExTests()
+        {
+            RegExDfa<char> regExDfa = new RegExDfa<char>(RegExFactory.Epsilon<char>(), 1);
+            CheckNullTransitionTests(regExDfa);
         }
     }
 }
