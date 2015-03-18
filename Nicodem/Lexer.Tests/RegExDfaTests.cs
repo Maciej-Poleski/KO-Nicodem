d347209e33f65c8795a5884651c20031f8c334f7
karolbanys <karol.banys@gmail.com>
2015-03-18 10:12:03 +0100

    Add class RegExDfaTests

---
+++ Nicodem/Lexer.Tests/RegExDfaTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Nicodem.Lexer;
+
+namespace Lexer.Tests
+{
+    [TestFixture]
+    class RegExDfaTests
+    {
+        //Create Dfa from empty RegEx
+        [Test]
+        public void EmptyRegexTests()
+        {
+            RegexDfa<char> regEx = new RegexDfa<char>(RegExFactory.Empty<char>(), 1);
+        }
+    }
+}
