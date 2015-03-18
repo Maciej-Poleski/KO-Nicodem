9df695e8e0911e8d605daea82435ebeec00598f1
karolbanys <karol.banys@gmail.com>
2015-03-18 13:46:30 +0100

    Add RegExDfa.cs to git

---
+++ Nicodem/Lexer/RegExDfa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nicodem.Lexer
+{
+    public class RegExDfa<T> : IDfa<DFAState<T>, T> where T : IComparable<T>, IEquatable<T>
+    {
+        private readonly uint accepting;
+
+        private readonly SortedDictionary<RegEx<T>, DFAState<T>> dictionaryOfDfaStates =
+            new SortedDictionary<RegEx<T>, DFAState<T>>();
+
+        public RegExDfa(RegEx<T> regEx, uint acceptingStateMarker)
+        {
+            accepting = acceptingStateMarker;
+            Start = CalculateDfaState(regEx);
+        }
+
+        public DFAState<T> Start { get; private set; }
+
+        private DFAState<T> CalculateDfaState(RegEx<T> regEx)
+        {
+            if (dictionaryOfDfaStates.ContainsKey(regEx))
+                return dictionaryOfDfaStates[regEx]; //return this state
+
+            var new_state = new DFAState<T>();
+            dictionaryOfDfaStates.Add(regEx, new_state);
+
+            var listOfTransitions = new List<KeyValuePair<T, DFAState<T>>>();
+
+            foreach (var c in regEx.DerivChanges())
+            {
+                listOfTransitions.Add(new KeyValuePair<T, DFAState<T>>(c, CalculateDfaState(regEx.Derivative(c))));
+            }
+
+            if (regEx.HasEpsilon())
+                new_state.Accepting = accepting;
+            else
+                new_state.Accepting = 0;
+
+            new_state.Transitions = listOfTransitions.ToArray();
+
+            return new_state;
+        }
+    }
+}
