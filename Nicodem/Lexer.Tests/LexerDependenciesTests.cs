fbf37ca0c0a88b3afd71e8f597bf9e33ad41347b
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2015-03-20 12:32:08 +0100

    Add explicit DFA from empty creation test

---
+++ Nicodem/Lexer.Tests/LexerDependenciesTests.cs
@@ -0,0 +1,17 @@
+﻿using Nicodem.Lexer;
+using NUnit.Framework;
+
+namespace Lexer.Tests
+{
+    [TestFixture]
+    internal class LexerDependenciesTests
+    {
+        [Test]
+        public void DfaConstructionFromRegex()
+        {
+            var regex = RegExFactory.Empty<char>();
+            var dfa = new RegExDfa<char>(regex, 1);
+            DfaUtils.DfaStatesNotNullConcpetCheck<RegExDfa<char>, DFAState<char>, char>.CheckDfaStatesNotNull(dfa);
+        }
+    }
+}
