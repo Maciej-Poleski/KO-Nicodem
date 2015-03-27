3c21088fc8effaeda46840af67e9699a2923cf4d
guspiel <guspiel@gmail.com>
2015-03-27 18:33:39 +0100

    Classes w/o impl.: LlConfiguration, LookaheadDfa, see #41

---
+++ Nicodem/Parser/LookaheadDfa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Nicodem.Lexer;
+using Strilanc.Value;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Nicodem.Parser
+{
+	public class LookaheadDfa : Dfa<ISymbol>
+	{
+		public ReadOnlyDictionary<uint, IEnumerable<May<ISymbol>>> Decisions { get; private set; }
+		// Decisions[AcceptingState.Accepting] is the result of lookahead.
+
+		public LookaheadDfa(DfaState<ISymbol> start,
+			ReadOnlyDictionary<uint, IEnumerable<May<ISymbol>>> decisions):
+		base(start)
+		{ 
+			Decisions = decisions;
+		}
+	}
+}
+
---
+++ Nicodem/Parser/LookaheadDfaBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Nicodem.Parser
+{
+	public class LookaheadDfaBuilder<TProduction> 
+		where TProduction : IProduction
+	{
+		public LookaheadDfaBuilder()
+		{
+		}
+
+		public LookaheadDfa Build(Grammar<TProduction> grammar)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+
