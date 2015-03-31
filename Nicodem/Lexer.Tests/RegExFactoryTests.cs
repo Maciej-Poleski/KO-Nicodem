adcf561cbf4420edd761e5e451c21ee19769a72a
karolbanys <karol.banys@gmail.com>
2015-03-31 17:44:10 +0200

    Add derivative test for concatination

--- Nicodem/Lexer.Tests/RegExFactoryTests.cs
+++ Nicodem/Lexer.Tests/RegExFactoryTests.cs
@@ -228,6 +228,17 @@ namespace Lexer.Tests
 			Assert.AreEqual (all, star);
 		}
 
+        [Test]
+        public void RegExStarSimpleDerivativeTest()
+        {
+            var aStar = RegExFactory.Star<char>(RegExFactory.Intersection<char>(RegExFactory.Range<char>('a'), RegExFactory.Complement<char>(RegExFactory.Range<char>('b'))));
+            var bStar = RegExFactory.Star<char>(RegExFactory.Intersection<char>(RegExFactory.Range<char>('b'), RegExFactory.Complement<char>(RegExFactory.Range<char>('c'))));
+            var cStar = RegExFactory.Star<char>(RegExFactory.Intersection<char>(RegExFactory.Range<char>('c'), RegExFactory.Complement<char>(RegExFactory.Range<char>('d'))));
+            var dStar = RegExFactory.Star<char>(RegExFactory.Intersection<char>(RegExFactory.Range<char>('d'), RegExFactory.Complement<char>(RegExFactory.Range<char>('e'))));
+            var aStarbStarcStar = RegExFactory.Concat<char>(aStar, bStar, cStar, dStar);
+            Assert.AreEqual(dStar, aStarbStarcStar.Derivative('c'));
+        }
+
 		#endregion
 
 		#region Complement
