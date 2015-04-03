df2cc19c32095c79ab5f0004592a973bb809c7e8
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2015-04-03 15:52:00 +0200

    Prepared explicit token categories

--- Nicodem/Semantics/Grammar/NicodemGrammarProductions.cs
+++ Nicodem/Semantics/Grammar/NicodemGrammarProductions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,34 @@ namespace Nicodem.Semantics.Grammar
 {
     internal static class NicodemGrammarProductions
     {
-        internal static int TokenCategoriesCount { get { return TokenCategory.NextCategoryId; } }
+        internal static int TokenCategoriesCount
+        {
+            get { return TokenCategory.NextCategoryId; }
+        }
+
+        /// <summary>
+        /// All (ordered) token categories Lexer should be aware of.
+        /// </summary>
+        internal static IReadOnlyList<string> TokenCategories
+        {
+            get
+            {
+                Debug.Assert(TokenCategory.Categories.Count == TokenCategoriesCount);
+                return TokenCategory.Categories.ConvertAll(tc => tc.Regex);
+            }
+        }
+
+        #region TokenCategoryImplementation
+
         private struct TokenCategory
         {
             public int Category { get; private set; }
             public string Regex { get; private set; }
 
             internal static int NextCategoryId { get; private set; }
+            internal static Dictionary<string, TokenCategory> ImplicitTokenCategories { get; private set; }
+
+            internal static List<TokenCategory> Categories { get; private set; }
 
             public TokenCategory(int category, string regex) : this()
             {
@@ -25,13 +47,35 @@ namespace Nicodem.Semantics.Grammar
             static TokenCategory()
             {
                 NextCategoryId = 0;
+                ImplicitTokenCategories = new Dictionary<string, TokenCategory>();
+                Categories = new List<TokenCategory>();
             }
 
             public static implicit operator TokenCategory(string regex)
             {
-                return new TokenCategory(NextCategoryId++,regex);
+                var result = new TokenCategory(NextCategoryId++, regex);
+                Categories.Add(result);
+                return result;
             }
         }
+
+        private static TokenCategory T(this string token)
+        {
+            if (TokenCategory.ImplicitTokenCategories.ContainsKey(token))
+            {
+                return TokenCategory.ImplicitTokenCategories[token];
+            }
+            else
+            {
+                TokenCategory result = token;
+                TokenCategory.ImplicitTokenCategories.Add(token, result);
+                return result;
+            }
+        }
+
+        #endregion
+
+        #region ExplicitTokens
 
         // Comments
         private static TokenCategory LineCommentCStyle = "//(^\n)*\n";
@@ -42,13 +86,19 @@ namespace Nicodem.Semantics.Grammar
         private static TokenCategory TypeName = "(^[:space:])+";
 
         // TODO operators (remember - left recursion is forbidden)
+        // using T(this string) extension to shorten code
 
         // Literal values (atomic expression)
         private static TokenCategory DecimalNumberLiteral = "[:digit:]+"; // Only decimal number literals for now
-        private static TokenCategory CharacterLiteral = "'[:print:]'";
-        private static TokenCategory StringLiteral = "\"(^((^\\)\"))*(^\\)\"";  // String literal is delimited by not escaped " character
+        private static TokenCategory CharacterLiteral = "'(\\[:print:])|(^')'";
+
+        private static TokenCategory StringLiteral = "\"(^((^\\)\"))*(^\\)\"";
+            // String literal is delimited by not escaped " character
+
         private static TokenCategory BooleanLiteral = "true|false";
         // Inject 'name resolution' functionality with above literals families to avoid ambiguity
         private static TokenCategory ObjectName = "(^[:space:])+";
+
+        #endregion ExplicitTokens
     }
 }
