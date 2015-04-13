e433f1e5b7fcd77e305fc300f9ea2e8b2da62683
KarolB <karol.banys@gmail.com>
2015-04-13 13:20:40 +0200

    Add factory for types in NamedTypeNode

--- Nicodem/Semantics/AST/NamedTypeNode.cs
+++ Nicodem/Semantics/AST/NamedTypeNode.cs
@@ -6,6 +6,33 @@ namespace Nicodem.Semantics.AST
 	{
 		public string Name { get; set; }
 
+        public static NamedTypeNode VoidType(){
+            NamedTypeNode node = new NamedTypeNode();
+            node.Name = "void";
+            return node;
+        }
+
+        public static NamedTypeNode BoolType()
+        {
+            NamedTypeNode node = new NamedTypeNode();
+            node.Name = "bool";
+            return node;
+        }
+
+        public static NamedTypeNode IntType()
+        {
+            NamedTypeNode node = new NamedTypeNode();
+            node.Name = "int";
+            return node;
+        }
+
+        public static NamedTypeNode CharType()
+        {
+            NamedTypeNode node = new NamedTypeNode();
+            node.Name = "char";
+            return node;
+        }
+
 		public override void Accept (AbstractVisitor visitor)
 		{
 			visitor.Visit (this);
