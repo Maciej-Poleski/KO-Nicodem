a35cd16e37ff5c8dcb852222dbebf948b66e7e13
marcin.dziadus <marcin.dziadus@bitbucket.org>
2015-04-17 20:05:54 +0200

    minor fix

--- Nicodem/Lexer/CharactersClasses.cs
+++ Nicodem/Lexer/CharactersClasses.cs
@@ -7,7 +7,7 @@ namespace Nicodem.Lexer
     public struct CharactersClasses
     {
         public static readonly RegEx<char> digit = RegExFactory.Range('0', (char)('9' + 1));
-        public static readonly RegEx<char> print = RegExFactory.Union(RegExFactory.Range((char)0, (char)32), RegExFactory.Range((char)127, (char)160));
+        public static readonly RegEx<char> print = RegExFactory.Complement(RegExFactory.Union(RegExFactory.Range((char)0, (char)32), RegExFactory.Range((char)127, (char)160)));
         public static readonly RegEx<char> space = RegExFactory.Union(RegExFactory.Range('\t', (char)('\t' + 1)),
                                                        RegExFactory.Range('\r', (char)('\r' + 1)),
                                                        RegExFactory.Range('\n', (char)('\n' + 1)),
