d925987e86a44a6b6e1d138009c03f4d41f4b4c0
marcin.dziadus <marcin.dziadus@bitbucket.org>
2015-04-18 22:16:38 +0200

    Clean up.

--- Nicodem/Semantics/CST/CSTBuilder.cs
+++ Nicodem/Semantics/CST/CSTBuilder.cs
@@ -4,32 +4,33 @@ using Nicodem.Semantics.Grammar;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Nicodem.Semantics
+namespace Nicodem.Semantics.CST
 {
     internal static class CSTBuilder
     {
-        internal static IParseTree<Symbol> buildCST(Source.IOrigin origin)
+        internal static IParseTree<Symbol> build(Source.IOrigin origin)
         {
             var categories = NicodemGrammarProductions.TokenCategories;
             var whiteSpaceCategories = NicodemGrammarProductions.WhiteSpaceTokenCategories;
 
             var forbidden = new List<int>(whiteSpaceCategories.Count);
             
-            for(int i = 0, idx = 0; i < categories.Count; ++i)
-                if(whiteSpaceCategories.Contains(categories[i])) forbidden[idx++] = i;
+            for (int i = 0, idx = 0; i < categories.Count; ++i)
+                if (whiteSpaceCategories.Contains(categories[i]))
+                    forbidden[idx++] = i;
 
             var regExCategories = categories.Select(c => RegExParser.Parse(c));
 
             var lexer = new Nicodem.Lexer.Lexer(regExCategories.ToArray());
 
             var tokens = lexer.Process(origin).ToList();
-            var sanitizedTokens = tokens.Where(a => a.Item2.All(c => ! forbidden.Contains(c)));
+            var sanitizedTokens = tokens.Where(a => a.Item2.All(c => !forbidden.Contains(c)));
 
             var leafs = tokens.Select(r => r.Item2.Select(s => new ParseLeaf<Symbol>(r.Item1, new Symbol(s))));
 
             var grammar = new Grammar<Symbol>(
-                NicodemGrammarProductions.StartSymbol(),
-                NicodemGrammarProductions.MakeProductionsDictionaryForGrammarConstructor());
+                              NicodemGrammarProductions.StartSymbol(),
+                              NicodemGrammarProductions.MakeProductionsDictionaryForGrammarConstructor());
 
             var parser = new LLStarParser<Symbol>(grammar);
             var cst = parser.Parse(leafs);
