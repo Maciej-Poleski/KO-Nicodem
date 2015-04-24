d83eae3d813efdf2696992ce85a1ccf93aea54de
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2015-04-24 15:49:15 +0200

    FunctionSplitter is ready fixes #66

---
+++ Nicodem/Semantics/Visitors/FunctionSplitterVisitor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Nicodem.Semantics.AST;
+
+namespace Nicodem.Semantics.Visitors
+{
+    internal class FunctionSplitterVisitor : AbstractRecursiveVisitor
+    {
+        private readonly List<FunctionDefinitionExpression> _functions = new List<FunctionDefinitionExpression>();
+
+        internal IReadOnlyList<FunctionDefinitionExpression> Functions
+        {
+            get { return _functions; }
+        }
+
+        public override void Visit(FunctionDefinitionExpression node)
+        {
+            _functions.Add(node);
+            base.Visit(node);
+        }
+    }
+
+    public static partial class Extensions
+    {
+        internal static IReadOnlyCollection<FunctionDefinitionExpression> GetAllFunctionDefinitions(
+            this ProgramNode node)
+        {
+            var visitor = new FunctionSplitterVisitor();
+            node.Accept(visitor);
+            return visitor.Functions;
+        }
+    }
+}
