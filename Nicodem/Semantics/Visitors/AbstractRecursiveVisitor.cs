428942c79656aee0aeb9af5765c600ae275fe657
Patryk Mikos <usciskajkaktusa@gmail.com>
2015-04-24 19:04:18 +0200

    AST.Visitor NullReferenceException fix

--- Nicodem/Semantics/Visitors/AbstractRecursiveVisitor.cs
+++ Nicodem/Semantics/Visitors/AbstractRecursiveVisitor.cs
@@ -8,14 +8,18 @@ namespace Nicodem.Semantics.Visitors
 		public override void Visit(ProgramNode node)
 		{
 			base.Visit (node);
-			foreach (var child in node.Functions)
-				child.Accept (this);
+
+			if (!ReferenceEquals(node.Functions, null))
+				foreach (var child in node.Functions)
+					if (!ReferenceEquals(child, null))
+						child.Accept (this);
 		}
 
 		public override void Visit(ExpressionNode node) 
 		{
 			base.Visit (node);
-			node.ExpressionType.Accept (this);
+			if (!ReferenceEquals(node.ExpressionType, null))
+				node.ExpressionType.Accept (this);
 		}
 		#endregion
 
@@ -23,7 +27,8 @@ namespace Nicodem.Semantics.Visitors
 		public override void Visit(ArrayTypeNode node)
 		{
 			base.Visit (node);
-			node.ElementType.Accept (this);
+			if (!ReferenceEquals(node.ElementType, null))
+				node.ElementType.Accept (this);
 		}
 		#endregion
 
@@ -32,51 +37,77 @@ namespace Nicodem.Semantics.Visitors
         public override void Visit(FunctionDefinitionExpression node)
         {
             base.Visit(node);
-            node.ResultType.Accept(this);
-            node.Body.Accept(this);
-            foreach (var parameter in node.Parameters)
-                parameter.Accept(this);
+
+			if (!ReferenceEquals(node.ResultType, null))
+				node.ResultType.Accept(this);
+
+			if (!ReferenceEquals(node.Body, null))
+				node.Body.Accept(this);
+            
+			if (!ReferenceEquals(node.Parameters, null))
+				foreach (var parameter in node.Parameters)
+					if (!ReferenceEquals(parameter, null))
+						parameter.Accept(this);
         }
 
 		public override void Visit(ArrayNode node)
 		{
 			base.Visit (node);
-			foreach (var child in node.Elements)
-				child.Accept (this);
+
+			if (!ReferenceEquals(node.Elements, null))
+				foreach (var child in node.Elements)
+					if (!ReferenceEquals(child, null))
+						child.Accept (this);
 		}
 
         public override void Visit(AtomNode node)
         {
             base.Visit(node);
-            node.VariableType.Accept(this);
+			if (!ReferenceEquals(node.VariableType, null))
+				node.VariableType.Accept(this);
         }
 
 		public override void Visit(BlockExpressionNode node)
 		{
 			base.Visit (node);
-			foreach (var child in node.Elements)
-				child.Accept (this);
+
+			if (!ReferenceEquals(node.Elements, null))
+				foreach (var child in node.Elements)
+					if (!ReferenceEquals(child, null))
+						child.Accept (this);
 		}
 
 		public override void Visit(ElementNode node)
 		{
 			base.Visit (node);
-			node.Array.Accept (this);
-			node.Index.Accept (this);
+
+			if (!ReferenceEquals(node.Array, null))
+				node.Array.Accept (this);
+
+			if (!ReferenceEquals(node.Index, null))
+				node.Index.Accept (this);
 		}
 
 		public override void Visit(FunctionCallNode node)
 		{
 			base.Visit (node);
-			foreach (var arg in node.Arguments)
-				arg.Accept (this);
+
+			if (!ReferenceEquals(node.Arguments, null))
+				foreach (var arg in node.Arguments)
+					if (!ReferenceEquals(arg, null))
+						arg.Accept (this);
 		}
 
 		public override void Visit(IfNode node)
 		{
 			base.Visit (node);
-			node.Condition.Accept (this);
-			node.Then.Accept (this);
+
+			if (!ReferenceEquals(node.Condition, null))
+				node.Condition.Accept (this);
+
+			if (!ReferenceEquals(node.Then, null))
+				node.Then.Accept (this);
+
 			if (node.HasElse)
 				node.Else.Accept (this);
 		}
@@ -84,22 +115,30 @@ namespace Nicodem.Semantics.Visitors
 		public override void Visit(LoopControlNode node)
 		{
 			base.Visit (node);
-			node.Value.Accept (this);
+			if (!ReferenceEquals(node.Value, null))
+				node.Value.Accept (this);
 		}
 
 		public override void Visit(OperationNode node)
 		{
 			base.Visit (node);
-			foreach (var arg in node.Arguments)
-				arg.Accept (this);
+
+			if (!ReferenceEquals(node.Arguments, null))
+				foreach (var arg in node.Arguments)
+					if (!ReferenceEquals(arg, null))
+						arg.Accept (this);
 		}
 
 		public override void Visit(SliceNode node)
 		{
 			base.Visit (node);
-			node.Array.Accept (this);
+
+			if (!ReferenceEquals(node.Array, null))
+				node.Array.Accept (this);
+
 			if (node.HasLeft)
 				node.Left.Accept (this);
+
 			if (node.HasRight)
 				node.Right.Accept (this);
 		}
@@ -107,7 +146,8 @@ namespace Nicodem.Semantics.Visitors
         public override void Visit(VariableDeclNode node)
         {
             base.Visit(node);
-            node.Type.Accept(this);
+			if (!ReferenceEquals(node.Type, null))
+				node.Type.Accept(this);
         }
 
         #region VariableDeclNode direct children
@@ -115,7 +155,8 @@ namespace Nicodem.Semantics.Visitors
         public override void Visit(VariableDefNode node)
 		{
 			base.Visit (node);
-			node.Value.Accept (this);
+			if (!ReferenceEquals(node.Value, null))
+				node.Value.Accept (this);
 		}
 
         #endregion
@@ -123,14 +164,20 @@ namespace Nicodem.Semantics.Visitors
 		public override void Visit(VariableUseNode node)
 		{
 			base.Visit (node);
-			node.Declaration.Accept (this);
+			if (!ReferenceEquals(node.Declaration, null))
+				node.Declaration.Accept (this);
 		}
 
 		public override void Visit(WhileNode node)
 		{
 			base.Visit (node);
-			node.Condition.Accept (this);
-			node.Body.Accept (this);
+
+			if (!ReferenceEquals(node.Condition, null))
+				node.Condition.Accept (this);
+
+			if (!ReferenceEquals(node.Body, null))
+				node.Body.Accept (this);
+
 			if (node.HasElse)
 				node.Else.Accept (this);
 		}
