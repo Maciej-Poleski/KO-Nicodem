ea89fb257c0b187e8751a9ed9af107d63e5ccebf
guspiel <guspiel@gmail.com>
2015-04-26 21:57:06 +0200

    Side effect extracion progress, see #62

---
+++ Nicodem/Semantics/SideEffectExtractor.cs
@@ -0,0 +1,22 @@
+﻿using Nicodem.Semantics.AST;
+using Nicodem.Semantics.Visitors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nicodem.Semantics
+{
+    class SideEffectExtractor
+    {
+        private List<ExpressionNode> trees = new List<ExpressionNode>();
+
+        IEnumerable<ExpressionNode> Extract(ExpressionNode root)
+        {
+            var visitor = new SideEffectExtractorVisitor();
+            visitor.Visit(root);
+            return visitor.Trees;
+        }
+    }
+}
