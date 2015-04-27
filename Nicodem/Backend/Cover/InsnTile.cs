3fbc94144832357739cacb1c8a4d6289fe1e5ce8
Patryk Mikos <usciskajkaktusa@gmail.com>
2015-04-27 16:29:47 +0200

    Tile not implemented draft

---
+++ Nicodem/Backend/Cover/InsnTile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Nicodem.Backend.Representation;
+
+namespace Nicodem.Backend
+{
+	public class InsnTile
+	{
+		public IEnumerable<Instruction> Cover(Node node) {
+			throw new NotImplementedException ();
+		}
+	}
+}
+
---
+++ Nicodem/Backend/Cover/Instruction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nicodem.Backend
+{
+	public class Instruction
+	{
+		// TODO
+		public object RegisterUsed { get { throw new NotImplementedException (); } }
+
+		// TODO
+		public object RegisterDefined { get { throw new NotImplementedException (); } }
+
+		// TODO
+		public bool IsCopyInstruction { get { throw new NotImplementedException (); } }
+
+		// TODO
+		public string ToString(IReadOnlyDictionary<object, object> registerMapping) {
+			throw new NotImplementedException ();
+		}
+	}
+}
+
