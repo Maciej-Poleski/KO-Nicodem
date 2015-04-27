4db4fb9cd8b336b537355d93ba8c22794ac3ea4f
Patryk Mikos <usciskajkaktusa@gmail.com>
2015-04-27 17:18:31 +0200

    InsnTile fields

--- Nicodem/Backend/Cover/InsnTile.cs
+++ Nicodem/Backend/Cover/InsnTile.cs
@@ -6,6 +6,18 @@ namespace Nicodem.Backend
 {
 	public class InsnTile
 	{
+		readonly RegisterNode temporaryRegister = new TemporaryNode ();
+		readonly Func<RegisterNode, Node, IEnumerable<Instruction>> instructionBuilder;
+
+		Type Type { get; private set; }
+		IEnumerable<InsnTile> Children { get; private set; }
+
+		public InsnTile(Type type, IEnumerable<InsnTile> children, Func<RegisterNode, Node, IEnumerable<Instruction>> instructionBuilder) {
+			Type = type;
+			Children = children;
+			this.instructionBuilder = instructionBuilder;
+		}
+
 		public IEnumerable<Instruction> Cover(Node node) {
 			throw new NotImplementedException ();
 		}
