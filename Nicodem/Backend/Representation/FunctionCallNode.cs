35667ed2b66a87d314a62c054dae4291a813fcf3
Marcin Ziemiński <zieminn@gmail.com>
2015-05-06 19:40:30 +0200

    Some progress in Backend Node builder

--- Nicodem/Backend/Representation/FunctionCallNode.cs
+++ Nicodem/Backend/Representation/FunctionCallNode.cs
@@ -2,16 +2,16 @@
 
 namespace Nicodem.Backend.Representation
 {
-    public class FunctionCallNode<TFunction> : Node
+    public class FunctionCallNode : Node
     {
-        public FunctionCallNode(TFunction function, IReadOnlyList<Node> arguments)
+        public FunctionCallNode(Function function, IReadOnlyList<Node> arguments)
         {
             Function = function;
             Arguments = arguments;
         }
 
         // Function type depends on architecture. Adjust when Target will be in shape.
-        public TFunction Function { get; private set; }
+        public Function Function { get; private set; }
         public IReadOnlyList<Node> Arguments { get; private set; }
     }
 }
--- Nicodem/Semantics/Backend/NodeBuilder.cs
+++ Nicodem/Semantics/Backend/NodeBuilder.cs
@@ -39,7 +39,7 @@ namespace Nicodem.Semantics
 				return new Brep.SequenceNode(seq, null); // TODO maybe set that jump later?
 			}
 
-			public Brep.Node Build(ConstNode blockNode)
+			public Brep.Node Build(ConstNode constNode)
 			{
 				throw new NotImplementedException();
 			}
@@ -50,23 +50,90 @@ namespace Nicodem.Semantics
 				foreach(var arg in funCallNode.Arguments) {
 					args.Add(Build(arg as dynamic));
 				}
-				// TODO conflict - in ast a class, in backand a generic parameter
-				throw new NotImplementedException();
+				return new Brep.FunctionCallNode(funCallNode.Definition.BackendFunction, args);
 			}
 
 			public Brep.Node Build(OperatorNode opNode)
 			{
-				// TODO not enaugh more operator types
-                // @ziemin: This whole class should be a visitor... but i like your idea
+				if(opNode.Operator.CompareTo(OperatorType.PRE_INCREMENT) < 0) {
+					return BuildBinaryOperator(opNode);
+				} else {
+					return BuildUnaryOperator(opNode);
+				}
+			}
+
+			private Brep.Node BuildBinaryOperator(OperatorNode opNode) 
+			{
 				var leftArg = Build(opNode.Arguments.ElementAt(0) as dynamic);
 				var rightArg = Build(opNode.Arguments.ElementAt(1) as dynamic);
 
 				switch(opNode.Operator) {
 
 				case OperatorType.ASSIGN:
-					throw new NotImplementedException();
-				case OperatorType.PLUS:
-					return new Brep.AddOperatorNode(leftArg, rightArg);
+				case OperatorType.PLUS_ASSIGN:
+				case OperatorType.MINUS_ASSIGN:
+				case OperatorType.MUL_ASSIGN:
+				case OperatorType.DIV_ASSIGN:
+				case OperatorType.MOD_ASSIGN:
+				case OperatorType.BIT_SHIFT_UP_ASSIGN:
+				case OperatorType.BIT_SHIFT_DOWN_ASSIGN:
+				case OperatorType.BIT_AND_ASSIGN:
+				case OperatorType.BIT_XOR_ASSIGN:
+				case OperatorType.BIT_OR_ASSIGN:
+		        // "||"
+				case OperatorType.OR:
+		        // "&&"
+				case OperatorType.AND:
+		        // "|"
+				case OperatorType.BIT_OR:
+		        // "^"
+				case OperatorType.BIT_XOR:
+		        // "&"
+				case OperatorType.BIT_AND:
+		        // "==", "!="
+				case OperatorType.EQUAL:
+				case OperatorType.NOT_EQUAL:
+		        // "< <= > >="
+				case OperatorType.LESS:
+				case OperatorType.LESS_EQUAL:
+				case OperatorType.GREATER:
+				case OperatorType.GREATER_EQUAL:
+		        // "<< >>"
+				case OperatorType.BIT_SHIFT_UP:
+				case OperatorType.BIT_SHIFT_DOWN:
+		        // "+ -"
+				case OperatorType.PLUS: 
+				case OperatorType.MINUS:
+		        // "* / %"
+				case OperatorType.MUL:
+				case OperatorType.DIV:
+				case OperatorType.MOD:
+					break;
+
+				default:
+					throw new InvalidOperationException("Wrong operator!");
+				}
+				throw new NotImplementedException();
+			}
+
+			private Brep.Node BuildUnaryOperator(OperatorNode opNode) 
+			{
+				var arg = Build(opNode.Arguments.ElementAt(0) as dynamic);
+				switch(opNode.Operator) {
+		        // PRE "++ -- + - ! ~"
+				case OperatorType.PRE_INCREMENT:
+				case OperatorType.PRE_DECREMENT:
+				case OperatorType.UNARY_PLUS:
+				case OperatorType.UNARY_MINUS:
+				case OperatorType.NOT:
+				case OperatorType.BIT_NOT:
+		        // POST "++", "--"
+				case OperatorType.POST_INCREMENT:
+				case OperatorType.POST_DECREMENT:
+					break;
+
+				default:
+					throw new InvalidOperationException("Wrong operator!");
 				}
 				throw new NotImplementedException();
 			}
