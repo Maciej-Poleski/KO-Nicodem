1a7f6a29b4ebd2b898d5792cde57a68e8dbebd3f
Andrzej Dorobisz <andrzejdoro@gmail.com>
2015-05-11 11:21:47 +0200

    RegisterAllocator interface - see #94

---
+++ Nicodem/Backend/Builder/RegisterAllocator.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using Nicodem.Backend.Representation;
+
+namespace Nicodem.Backend.Builder
+{
+    public class RegisterAllocator
+    {
+        public RegisterAllocator()
+        {
+        }
+
+        public bool AllocateRegisters(TemporariesGraph interferenceGraph, TemporariesGraph copiesGraph, int availableRegisters){
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyDictionary<Temporary, int> TempsMapping { 
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IReadOnlyList<Temporary> TempsToMemory { 
+            get {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
+
