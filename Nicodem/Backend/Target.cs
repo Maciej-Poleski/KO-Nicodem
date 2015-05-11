fad73eb78d1252866fd1c7b9469d95752ce6d37e
Patryk Mikos <usciskajkaktusa@gmail.com>
2015-05-11 17:22:28 +0200

    x86-64 hardware registers

--- Nicodem/Backend/Target.cs
+++ Nicodem/Backend/Target.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using Nicodem.Backend.Representation;
 
 namespace Nicodem.Backend
 {
 	public class Target
 	{
+		public static readonly HardwareRegisterNode RAX = new HardwareRegisterNode ("rax");
+		public static readonly HardwareRegisterNode RBX = new HardwareRegisterNode ("rbx");
+		public static readonly HardwareRegisterNode RCX = new HardwareRegisterNode ("rcx");
+		public static readonly HardwareRegisterNode RDX = new HardwareRegisterNode ("rdx");
+		public static readonly HardwareRegisterNode RSP = new HardwareRegisterNode ("rsp");
+		public static readonly HardwareRegisterNode RBP = new HardwareRegisterNode ("rbp");
+		public static readonly HardwareRegisterNode RSI = new HardwareRegisterNode ("rsi");
+		public static readonly HardwareRegisterNode RDI = new HardwareRegisterNode ("rdi");
+		public static readonly HardwareRegisterNode R9  = new HardwareRegisterNode ("r9");
+		public static readonly HardwareRegisterNode R10 = new HardwareRegisterNode ("r10");
+		public static readonly HardwareRegisterNode R11 = new HardwareRegisterNode ("r11");
+		public static readonly HardwareRegisterNode R12 = new HardwareRegisterNode ("r12");
+		public static readonly HardwareRegisterNode R13 = new HardwareRegisterNode ("r13");
+		public static readonly HardwareRegisterNode R14 = new HardwareRegisterNode ("r14");
+		public static readonly HardwareRegisterNode R15 = new HardwareRegisterNode ("r15");
+		public static readonly HardwareRegisterNode R16 = new HardwareRegisterNode ("r16");
+
 		public Target () {
 		}
 
