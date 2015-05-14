4c490e62de6b61d1f990cc70a45a202094ee44ef
karolbanys <karol.banys@gmail.com>
2015-05-14 15:45:06 +0200

    Add interface to Extractor

--- Nicodem/Semantics/Extractors/ControlFlowExtractor.cs
+++ Nicodem/Semantics/Extractors/ControlFlowExtractor.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nicodem.Semantics.ExpressionGraph;
+using Nicodem.Semantics.AST;
+using Nicodem.Semantics.Visitors;
 
 namespace Nicodem.Semantics
 {
     class ControlFlowExtractor
     {
+        public Vertex Extract(ExpressionNode root)
+        {
+            var visitor = new ControlFlowVisitor();
+            visitor.Visit(root);
+            return visitor.Start;
+        }
     }
 }
