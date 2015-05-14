2cf7f4df0b8ac9b7b96338092ad5e6dffe7173f0
marcin.dziadus <marcin.dziadus@bitbucket.org>
2015-05-14 10:45:29 +0200

    Missing visitors added.

--- Nicodem/Backend/Representation/AbstractRecursiveVisitor.cs
+++ Nicodem/Backend/Representation/AbstractRecursiveVisitor.cs
@@ -1,213 +1,228 @@
 ﻿namespace Nicodem.Backend.Representation
 {
-    public abstract class AbstractRecursiveVisitor : AbstractVisitor
-    {
-        public override void Visit(Node node)
-        {
-        }
+	public abstract class AbstractRecursiveVisitor : AbstractVisitor
+	{
+		public override void Visit (Node node)
+		{
+		}
 
-        #region Node subclasses
+		#region Node subclasses
 
-        public override void Visit(LocationNode node)
-        {
-            Visit(node as Node);
-        }
+		public override void Visit (LabelNode node)
+		{
+			Visit (node as Node);
+		}
 
-        public override void Visit(ConditionalJumpNode node)
-        {
-            Visit(node as Node);
-            node.Condition.Accept(this);
-        }
+		public override void Visit (ConditionalJumpToLabelNode node)
+		{
+			Visit (node as Node);
+			node.Condition.Accept (this);
+		}
 
-        public override void Visit(SequenceNode node)
-        {
-            Visit(node as Node);
-            foreach (var chld in node.Sequence)
-            {
-                chld.Accept(this);
-            }
-        }
+		public override void Visit (UnconditionalJumpToLabelNode node)
+		{
+			Visit (node as Node);
+		}
 
-        public override void Visit(AssignmentNode node)
-        {
-            Visit(node as Node);
-            node.Source.Accept(this);
-            node.Target.Accept(this);
-        }
+		public override void Visit (LocationNode node)
+		{
+			Visit (node as Node);
+		}
 
-        public override void Visit(FunctionCallNode node)
-        {
-            Visit(node as Node);
-        }
+		public override void Visit (ConditionalJumpNode node)
+		{
+			Visit (node as Node);
+			node.Condition.Accept (this);
+		}
 
-        public override void Visit<TConstant>(ConstantNode<TConstant> node)
-        {
-            Visit(node as Node);
-        }
+		public override void Visit (SequenceNode node)
+		{
+			Visit (node as Node);
+			foreach (var chld in node.Sequence) {
+				chld.Accept (this);
+			}
+		}
 
-        public override void Visit(OperatorNode node)
-        {
-            Visit(node as Node);
-        }
+		public override void Visit (AssignmentNode node)
+		{
+			Visit (node as Node);
+			node.Source.Accept (this);
+			node.Target.Accept (this);
+		}
 
-        #region OperatorNode subclasses
+		public override void Visit (FunctionCallNode node)
+		{
+			Visit (node as Node);
+		}
 
-        public override void Visit(UnaryOperatorNode node)
-        {
-            Visit(node as OperatorNode);
-            node.Operand.Accept(this);
-        }
+		public override void Visit<TConstant> (ConstantNode<TConstant> node)
+		{
+			Visit (node as Node);
+		}
 
-        public override void Visit(BinaryOperatorNode node)
-        {
-            Visit(node as OperatorNode);
-            node.LeftOperand.Accept(this);
-            node.RightOperand.Accept(this);
-        }
+		public override void Visit (OperatorNode node)
+		{
+			Visit (node as Node);
+		}
 
-        #region UnaryOperatorNode subclasses
+		#region OperatorNode subclasses
 
-        public override void Visit(NegOperatorNode node)
-        {
-            Visit(node as UnaryOperatorNode);
-        }
+		public override void Visit (UnaryOperatorNode node)
+		{
+			Visit (node as OperatorNode);
+			node.Operand.Accept (this);
+		}
 
-        public override void Visit(BinNotOperatorNode node)
-        {
-            Visit(node as UnaryOperatorNode);
-        }
+		public override void Visit (BinaryOperatorNode node)
+		{
+			Visit (node as OperatorNode);
+			node.LeftOperand.Accept (this);
+			node.RightOperand.Accept (this);
+		}
 
-        public override void Visit(LogNotOperatorNode node)
-        {
-            Visit(node as UnaryOperatorNode);
-        }
+		#region UnaryOperatorNode subclasses
 
-        #endregion
+		public override void Visit (NegOperatorNode node)
+		{
+			Visit (node as UnaryOperatorNode);
+		}
 
-        #region BinaryOperatorNode subclasses
+		public override void Visit (BinNotOperatorNode node)
+		{
+			Visit (node as UnaryOperatorNode);
+		}
 
-        public override void Visit(AddOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (LogNotOperatorNode node)
+		{
+			Visit (node as UnaryOperatorNode);
+		}
 
-        public override void Visit(SubOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		#endregion
 
-        public override void Visit(MulOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		#region BinaryOperatorNode subclasses
 
-        public override void Visit(DivOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (AddOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(ModOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (SubOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(ShlOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (MulOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(ShrOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (DivOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(LtOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (ModOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(LteOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (ShlOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(GtOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (ShrOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(GteOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (LtOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(EqOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (LteOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(NeqOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (GtOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(BitAndOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (GteOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(BitXorOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (EqOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(BitOrOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (NeqOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(LogAndOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (BitAndOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(LogOrOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public override void Visit (BitXorOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        #endregion
+		public override void Visit (BitOrOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        #endregion
+		public override void Visit (LogAndOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        #region LocationNode subclasses
+		public override void Visit (LogOrOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public override void Visit(RegisterNode node)
-        {
-            Visit(node as LocationNode);
-        }
+		#endregion
 
-        public override void Visit(MemoryNode node)
-        {
-            Visit(node as LocationNode);
-        }
+		#endregion
 
-        #region RegisterNode subclasses
+		#region LocationNode subclasses
 
-        public override void Visit(HardwareRegisterNode node)
-        {
-            Visit(node as RegisterNode);
-        }
+		public override void Visit (RegisterNode node)
+		{
+			Visit (node as LocationNode);
+		}
 
-        public override void Visit(TemporaryNode node)
-        {
-            Visit(node as RegisterNode);
-        }
+		public override void Visit (MemoryNode node)
+		{
+			Visit (node as LocationNode);
+		}
 
-        #endregion
+		#region RegisterNode subclasses
 
-        #endregion
+		public override void Visit (HardwareRegisterNode node)
+		{
+			Visit (node as RegisterNode);
+		}
 
-        #endregion
-    }
+		public override void Visit (TemporaryNode node)
+		{
+			Visit (node as RegisterNode);
+		}
+
+		#endregion
+
+		#endregion
+
+		#endregion
+	}
 }
--- Nicodem/Backend/Representation/AbstractVisitor.cs
+++ Nicodem/Backend/Representation/AbstractVisitor.cs
@@ -1,203 +1,218 @@
 ﻿namespace Nicodem.Backend.Representation
 {
-    public abstract class AbstractVisitor
-    {
-        public virtual void Visit(Node node)
-        {
-        }
+	public abstract class AbstractVisitor
+	{
+		public virtual void Visit (Node node)
+		{
+		}
 
-        #region Node subclasses
+		#region Node subclasses
 
-        public virtual void Visit(LocationNode node)
-        {
-            Visit(node as Node);
-        }
+		public virtual void Visit (LabelNode node)
+		{
+			Visit (node as Node);
+		}
 
-        public virtual void Visit(ConditionalJumpNode node)
-        {
-            Visit(node as Node);
-        }
+		public virtual void Visit (ConditionalJumpToLabelNode node)
+		{
+			Visit (node as Node);
+		}
 
-        public virtual void Visit(SequenceNode node)
-        {
-            Visit(node as Node);
-        }
+		public virtual void Visit (UnconditionalJumpToLabelNode node)
+		{
+			Visit (node as Node);
+		}
 
-        public virtual void Visit(AssignmentNode node)
-        {
-            Visit(node as Node);
-        }
+		public virtual void Visit (LocationNode node)
+		{
+			Visit (node as Node);
+		}
 
-        public virtual void Visit(FunctionCallNode node)
-        {
-            Visit(node as Node);
-        }
+		public virtual void Visit (ConditionalJumpNode node)
+		{
+			Visit (node as Node);
+		}
 
-        public virtual void Visit<TConstant>(ConstantNode<TConstant> node)
-        {
-            Visit(node as Node);
-        }
+		public virtual void Visit (SequenceNode node)
+		{
+			Visit (node as Node);
+		}
 
-        public virtual void Visit(OperatorNode node)
-        {
-            Visit(node as Node);
-        }
+		public virtual void Visit (AssignmentNode node)
+		{
+			Visit (node as Node);
+		}
 
-        #region OperatorNode subclasses
+		public virtual void Visit (FunctionCallNode node)
+		{
+			Visit (node as Node);
+		}
 
-        public virtual void Visit(UnaryOperatorNode node)
-        {
-            Visit(node as OperatorNode);
-        }
+		public virtual void Visit<TConstant> (ConstantNode<TConstant> node)
+		{
+			Visit (node as Node);
+		}
 
-        public virtual void Visit(BinaryOperatorNode node)
-        {
-            Visit(node as OperatorNode);
-        }
+		public virtual void Visit (OperatorNode node)
+		{
+			Visit (node as Node);
+		}
 
-        #region UnaryOperatorNode subclasses
+		#region OperatorNode subclasses
 
-        public virtual void Visit(NegOperatorNode node)
-        {
-            Visit(node as UnaryOperatorNode);
-        }
+		public virtual void Visit (UnaryOperatorNode node)
+		{
+			Visit (node as OperatorNode);
+		}
 
-        public virtual void Visit(BinNotOperatorNode node)
-        {
-            Visit(node as UnaryOperatorNode);
-        }
+		public virtual void Visit (BinaryOperatorNode node)
+		{
+			Visit (node as OperatorNode);
+		}
 
-        public virtual void Visit(LogNotOperatorNode node)
-        {
-            Visit(node as UnaryOperatorNode);
-        }
+		#region UnaryOperatorNode subclasses
 
-        #endregion
+		public virtual void Visit (NegOperatorNode node)
+		{
+			Visit (node as UnaryOperatorNode);
+		}
 
-        #region BinaryOperatorNode subclasses
+		public virtual void Visit (BinNotOperatorNode node)
+		{
+			Visit (node as UnaryOperatorNode);
+		}
 
-        public virtual void Visit(AddOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (LogNotOperatorNode node)
+		{
+			Visit (node as UnaryOperatorNode);
+		}
 
-        public virtual void Visit(SubOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		#endregion
 
-        public virtual void Visit(MulOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		#region BinaryOperatorNode subclasses
 
-        public virtual void Visit(DivOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (AddOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(ModOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (SubOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(ShlOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (MulOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(ShrOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (DivOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(LtOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (ModOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(LteOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (ShlOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(GtOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (ShrOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(GteOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (LtOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(EqOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (LteOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(NeqOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (GtOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(BitAndOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (GteOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(BitXorOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (EqOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(BitOrOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (NeqOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(LogAndOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (BitAndOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(LogOrOperatorNode node)
-        {
-            Visit(node as BinaryOperatorNode);
-        }
+		public virtual void Visit (BitXorOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        #endregion
+		public virtual void Visit (BitOrOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        #endregion
+		public virtual void Visit (LogAndOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        #region LocationNode subclasses
+		public virtual void Visit (LogOrOperatorNode node)
+		{
+			Visit (node as BinaryOperatorNode);
+		}
 
-        public virtual void Visit(RegisterNode node)
-        {
-            Visit(node as LocationNode);
-        }
+		#endregion
 
-        public virtual void Visit(MemoryNode node)
-        {
-            Visit(node as LocationNode);
-        }
+		#endregion
 
-        #region RegisterNode subclasses
+		#region LocationNode subclasses
 
-        public virtual void Visit(HardwareRegisterNode node)
-        {
-            Visit(node as RegisterNode);
-        }
+		public virtual void Visit (RegisterNode node)
+		{
+			Visit (node as LocationNode);
+		}
 
-        public virtual void Visit(TemporaryNode node)
-        {
-            Visit(node as RegisterNode);
-        }
+		public virtual void Visit (MemoryNode node)
+		{
+			Visit (node as LocationNode);
+		}
 
-        #endregion
+		#region RegisterNode subclasses
 
-        #endregion
+		public virtual void Visit (HardwareRegisterNode node)
+		{
+			Visit (node as RegisterNode);
+		}
 
-        #endregion
-    }
+		public virtual void Visit (TemporaryNode node)
+		{
+			Visit (node as RegisterNode);
+		}
+
+		#endregion
+
+		#endregion
+
+		#endregion
+	}
 }
