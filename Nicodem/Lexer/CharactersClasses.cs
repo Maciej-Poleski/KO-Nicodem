543a54696ee232af8a709b8f41d88e0010348176
marcin.dziadus <marcin.dziadus@bitbucket.org>
2015-05-14 10:24:57 +0200

    Fix the [:space:] class

--- Nicodem/Lexer/CharactersClasses.cs
+++ Nicodem/Lexer/CharactersClasses.cs
@@ -8,7 +8,8 @@ namespace Nicodem.Lexer
     {
         public static readonly RegEx<char> digit = RegExFactory.Range('0', (char)('9' + 1));
         public static readonly RegEx<char> print = RegExFactory.Complement(RegExFactory.Union(RegExFactory.Range((char)0, (char)32), RegExFactory.Range((char)127, (char)160)));
-        public static readonly RegEx<char> space = RegExFactory.Union(RegExFactory.Range('\t', (char)('\t' + 1)),
+        public static readonly RegEx<char> space = RegExFactory.Union(RegExFactory.Range(' ', (char)(' ' + 1)),
+													   RegExFactory.Range('\t', (char)('\t' + 1)),
                                                        RegExFactory.Range('\r', (char)('\r' + 1)),
                                                        RegExFactory.Range('\n', (char)('\n' + 1)),
                                                        RegExFactory.Range('\v', (char)('\v' + 1)),
