a8d72044ccf08e9dde9dffd3ee21c70fc7138f3a
karolbanys <karol.banys@gmail.com>
2015-05-23 17:36:58 +0200

    Fix in Extractor

--- Nicodem/Semantics/Extractors/ControlFlowExtractor.cs
+++ Nicodem/Semantics/Extractors/ControlFlowExtractor.cs
@@ -11,10 +11,19 @@ namespace Nicodem.Semantics.Extractors
 {
     class ControlFlowExtractor
     {
-        public IEnumerable<Vertex> Extract(ExpressionNode root)
+        /// <summary>
+        /// Function which extract control flow from root.
+        /// In IEnumerable returned vertex there are rules:
+        /// 1. First Vertex it's begining of graph.
+        /// 2. Last Vertex has returned value.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="root">Root of AST tree.</param>
+        /// <returns>Vertex in transformed expression graph.</returns>
+        public IEnumerable<Vertex> Extract<T>(T root) where T : ExpressionNode
         {
             var visitor = new ControlFlowVisitor();
-            visitor.Visit(root);
+            root.Accept(visitor);
             return visitor.Graph;
         }
     }
