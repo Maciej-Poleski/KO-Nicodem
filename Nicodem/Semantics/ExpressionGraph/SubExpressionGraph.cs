ab2d803fac2d9d1639d600539121680256c7a5a5
karolbanys <karol.banys@gmail.com>
2015-05-25 22:16:15 +0200

    Add ReturnedAbstractVisitor and accept methods in nodes

---
+++ Nicodem/Semantics/ExpressionGraph/SubExpressionGraph.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nicodem.Semantics.ExpressionGraph
+{
+    class SubExpressionGraph
+    {
+        public Vertex Start;
+        public Vertex End{
+            get{
+                if(currentEnd.Count == 0)
+                    return null;
+                return currentEnd[0];
+            }
+        }
+        public List<Vertex> Graph = new List<Vertex>();
+        public List<Vertex> currentEnd = new List<Vertex>();
+
+        public SubExpressionGraph(Vertex _start, List<Vertex> _graph, List<Vertex> _currentEnd)
+        {
+            Start = _start;
+            Graph = _graph;
+            currentEnd = _currentEnd;
+        }
+
+        public SubExpressionGraph(){ }
+    }
+}
---
+++ Nicodem/Semantics/Visitors/ReturnedAbstractVisitor.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nicodem.Semantics.AST;
+
+namespace Nicodem.Semantics.Visitors
+{
+    class ReturnedAbstractVisitor<T>
+    {
+        public virtual T Visit(Node node)
+        {
+            return default(T);
+        }
+
+        #region Node direct children
+        public virtual T Visit(ProgramNode node)
+        {
+            return Visit(node as Node);
+        }
+
+        public virtual T Visit(TypeNode node)
+        {
+            return Visit(node as Node);
+        }
+
+        public virtual T Visit(ExpressionNode node)
+        {
+            return Visit(node as Node);
+        }
+        #endregion
+
+        #region TypeNode direct children
+        public virtual T Visit(NamedTypeNode node)
+        {
+           return Visit(node as TypeNode);
+        }
+        public virtual T Visit(ArrayTypeNode node)
+        {
+           return Visit(node as TypeNode);
+        }
+        #endregion
+
+        #region ExpressionNode direct children
+
+        public virtual T Visit(FunctionDefinitionNode node)
+        {
+           return Visit(node as ExpressionNode);
+        }
+
+        public virtual T Visit(ArrayNode node)
+        {
+           return Visit(node as ConstNode);
+        }
+
+        public virtual T Visit(AtomNode node)
+        {
+           return Visit(node as ExpressionNode);
+        }
+
+        public virtual T Visit(BlockExpressionNode node)
+        {
+           return Visit(node as ExpressionNode);
+        }
+
+        public virtual T Visit(ElementNode node)
+        {
+           return Visit(node as ExpressionNode);
+        }
+
+        public virtual T Visit(FunctionCallNode node)
+        {
+           return Visit(node as ExpressionNode);
+        }
+
+        public virtual T Visit(IfNode node)
+        {
+           return Visit(node as ExpressionNode);
+        }
+
+        public virtual T Visit(LoopControlNode node)
+        {
+           return Visit(node as ExpressionNode);
+        }
+
+        public virtual T Visit(SliceNode node)
+        {
+           return Visit(node as ExpressionNode);
+        }
+
+        public virtual T Visit(VariableDeclNode node)
+        {
+           return Visit(node as ExpressionNode);
+        }
+
+        #region VariableDeclNode direct children
+
+        public virtual T Visit(VariableDefNode node)
+        {
+           return Visit(node as VariableDeclNode);
+        }
+
+        #endregion
+
+        public virtual T Visit(VariableUseNode node)
+        {
+           return Visit(node as ExpressionNode);
+        }
+
+        public virtual T Visit(WhileNode node)
+        {
+           return Visit(node as ExpressionNode);
+        }
+        #endregion
+
+        public virtual T Visit(OperatorNode node)
+        {
+           return Visit(node as ExpressionNode);
+        }
+    }
+}
