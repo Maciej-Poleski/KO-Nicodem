b489c20af12c968060a85747f6a3055a4097fb85
guspiel <guspiel@gmail.com>
2015-06-08 11:32:31 +0200

    Equals with a switch to be oblivious to IsConstant

--- Nicodem/Semantics/AST/Node.cs
+++ Nicodem/Semantics/AST/Node.cs
@@ -26,6 +26,7 @@ namespace Nicodem.Semantics.AST
         // should call Compare in the base class.
         public sealed override bool Equals(object obj)
         {
+            if (obj == null) return false;
             if (GetType() != obj.GetType()) return false;
             return Compare(obj);
         }
