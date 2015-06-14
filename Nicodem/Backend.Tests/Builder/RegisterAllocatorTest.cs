8dff5bcc9ae5e252b2c83ec6af9523bb0a70ff61
marcin.dziadus <marcin.dziadus@bitbucket.org>
2015-06-14 23:17:45 +0200

    More challenging test for the allocator added.

--- Nicodem/Backend.Tests/Builder/RegisterAllocatorTest.cs
+++ Nicodem/Backend.Tests/Builder/RegisterAllocatorTest.cs
@@ -35,6 +35,40 @@ namespace Nicodem.Backend.Tests
 		}
 
 		[Test]
+		public void NonTrivialThreeColorableGraph (){
+			var registers = new List<HardwareRegisterNode> ();
+			registers.Add (new HardwareRegisterNode ("a"));
+			registers.Add (new HardwareRegisterNode ("b"));
+			registers.Add (new HardwareRegisterNode ("c"));
+
+			var v1 = new Vertex (new TemporaryNode ());
+			var v2 = new Vertex (new TemporaryNode ());
+			var v3 = new Vertex (new TemporaryNode ());
+			var v4 = new Vertex (new TemporaryNode ());
+			var v5 = new Vertex (new TemporaryNode ());
+			var v6 = new Vertex (new TemporaryNode ());
+			var v7 = new Vertex (registers[1]);
+			var v8 = new Vertex (new TemporaryNode ());
+			var v9 = new Vertex (new TemporaryNode ());
+
+			v1.NonCopyNeighbors.UnionWith (new HashSet<Vertex> (new Vertex[]{ v2, v3 }));
+			v2.NonCopyNeighbors.UnionWith (new HashSet<Vertex> (new Vertex[]{ v1, v3 }));
+			v3.NonCopyNeighbors.UnionWith (new HashSet<Vertex> (new Vertex[]{ v1, v2 }));
+			v4.NonCopyNeighbors.UnionWith (new HashSet<Vertex> (new Vertex[]{ v5, v6 }));
+			v5.NonCopyNeighbors.UnionWith (new HashSet<Vertex> (new Vertex[]{ v4, v6 }));
+			v6.NonCopyNeighbors.UnionWith (new HashSet<Vertex> (new Vertex[]{ v4, v5 }));
+			v7.NonCopyNeighbors.UnionWith (new HashSet<Vertex> (new Vertex[]{ v3, v4 }));
+			v8.NonCopyNeighbors.UnionWith (new HashSet<Vertex> (new Vertex[]{ v7, v9 }));
+			v9.NonCopyNeighbors.UnionWith (new HashSet<Vertex> (new Vertex[]{ v7, v8 }));
+
+			var graph = new InterferenceGraph (new List<Vertex> (new Vertex[9]{ v1, v2, v3, v4, v5, v6, v7, v8, v9 }));
+			var allocator = new RegisterAllocator (registers);
+			allocator.AllocateRegisters (graph);
+			Assert.AreEqual (IsValid (graph, allocator.RegistersColoring), true);
+			Assert.IsEmpty (allocator.SpilledRegisters);			
+		}
+
+		[Test]
 		public void SpillNeeded ()
 		{
 			var registers = new List<HardwareRegisterNode> ();
